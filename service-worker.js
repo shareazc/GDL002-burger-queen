2618cae31540c35f3a4af73ef6e649590962b6c3
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-05-28 10:57:59 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gdl002-burger-queen/precache-manifest.72af8638243175222e926e4af321feea.js"
+  "/gdl002-burger-queen/precache-manifest.8a338bad9ab2e4add1b37207d2015b75.js"
 );
 
 self.addEventListener('message', (event) => {
