0dcd92116339c6c9659d455476266f95f4303b32
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-05-28 08:54:13 -0500

    Updates

---
+++ precache-manifest.8a338bad9ab2e4add1b37207d2015b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f7fa73e6d13312de27ebfa3772caf4",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "5d116ebfacd16e1f1f38",
+    "url": "/gdl002-burger-queen/static/css/main.2fa4e22b.chunk.css"
+  },
+  {
+    "revision": "bc6b3c2130e71c6faece",
+    "url": "/gdl002-burger-queen/static/js/2.a66b6b59.chunk.js"
+  },
+  {
+    "revision": "5d116ebfacd16e1f1f38",
+    "url": "/gdl002-burger-queen/static/js/main.cf49a138.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  },
+  {
+    "revision": "8119e3b66cc46dde9ffc40a7a32d0c3c",
+    "url": "/gdl002-burger-queen/static/media/LogoBQ.8119e3b6.png"
+  }
+]);
